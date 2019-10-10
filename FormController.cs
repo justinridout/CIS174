759ed4a7e55aded9711f75c6f46f46134a3920b6
justinridout <56090823+justinridout@users.noreply.github.com>
2019-10-09 19:33:45 -0500

    Add files via upload

---
+++ FormController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CIS174_TestCoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIS174_TestCoreApp.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Index()
+        {
+            var viewModel = new CreatePersonPlusViewModel();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult CreatePerson(CreatePersonPlusViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Views/Form/Index.cshtml");
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
