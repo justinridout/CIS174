88773f41829d7214106d4d5401c4baebcd386273
justinridout <56090823+justinridout@users.noreply.github.com>
2019-10-16 19:09:38 -0500

    Add files via upload

---
+++ Api/HouseViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIS174_TestCoreApp.Models
+{
+    public class HouseViewModel
+    {
+        [Required]
+        public int id { get; set; }
+
+        [Required]
+        [Range(1,20)]
+        public int Bedrooms { get; set; }
+
+        [Required]
+        [Range(1, 3000)]
+        public int SquareFeet { get; set; }
+
+        [DataType(DataType.Date)]
+        public string DateBuilt { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string Flooring { get; set; }
+
+
+    }
+}
